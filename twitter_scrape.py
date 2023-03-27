6710a8d19a423332dbd656a2b211b6ac031e0d1f
dodzor <raul.ghergic@gmail.com>
2023-03-27 20:09:57 +0300

    open ai sketch

--- twitter_scrape.py
+++ twitter_scrape.py
@@ -5,11 +5,11 @@ import pandas as pd
 analyzer = SentimentIntensityAnalyzer()
 limit= 100 
 tweets_list =[] 
-for tweet in sntwitter.TwitterSearchScraper("Keto diet book Amazon").get_items(): 
+for tweet in sntwitter.TwitterSearchScraper("Keto diet Cookbook Amazon").get_items(): 
     if len(tweets_list)== limit: 
         break 
     else: 
-        tweets_list.append([tweet.date, tweet.content, tweet.retweetCount, tweet.likeCount])
+        tweets_list.append([tweet.date, tweet.rawContent, tweet.retweetCount, tweet.likeCount])
         # sentiment = analyzer.polarity_scores(tweet.content)
         # print(tweet.content)
         # print(sentiment)
@@ -24,12 +24,40 @@ df = pd.DataFrame(tweets_list,columns=["date", "tweets", "retweetcounts", "likec
 def sort_by_sentiment(df):
     df['sentiment_score'] = df['tweets'].apply(lambda x: analyzer.polarity_scores(x))
     df['sentiment_score'] = df['sentiment_score'].apply(lambda x: x['compound'])
+    df['link'] = df['tweets'].str.extract(r'(https?://t\.co/\w+)')
     df = df.sort_values(by='sentiment_score', ascending=False)
-    df = df.head(10)['tweets']
+    df = df.head(10)[['tweets', 'sentiment_score', 'link']]
     print(df)
     return df
 
-sort_by_sentiment(df)
+df = sort_by_sentiment(df)
+
+# book_title = df.loc[0,"tweets"]
+# sentiment = analyzer.polarity_scores(book_title)
+# sentiment = df.loc[0, "sentiment_score"]
+# print(book_title)
+# print(sentiment)
+
+
+# import openai
+
+# openai.api_key = ""
+
+# # "Summarize the item.description in two sentences".
+# prompt = "Summarize this in two sentences: " + book_title
+
+# book_summary = openai.Completion.create(
+#   model = "text-davinci-003",
+#   prompt = prompt,
+#   temperature = 1,
+#   max_tokens = 600,
+#   top_p = 1,
+#   frequency_penalty = 0,
+#   presence_penalty = 0
+# )
+
+# print(book_summary)
+
 
 # book_title = df.loc[0,"tweets"]
 # sentiment = analyzer.polarity_scores(book_title)
