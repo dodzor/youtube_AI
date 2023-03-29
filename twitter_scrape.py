2dc5fb8a175105c5f71b87c953df30bdd8d130cb
dodzor <raul.ghergic@gmail.com>
2023-03-29 21:48:49 +0300

    create book summary + generate video

--- twitter_scrape.py
+++ twitter_scrape.py
@@ -1,65 +1,91 @@
 import snscrape.modules.twitter as sntwitter
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import pandas as pd
+import openai
+import requests
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 analyzer = SentimentIntensityAnalyzer()
-limit= 100 
+limit= 10 
 tweets_list =[] 
-for tweet in sntwitter.TwitterSearchScraper("Keto diet Cookbook Amazon").get_items(): 
+for tweet in sntwitter.TwitterSearchScraper("Chinese history book Amazon").get_items(): 
     if len(tweets_list)== limit: 
         break 
     else: 
         tweets_list.append([tweet.date, tweet.rawContent, tweet.retweetCount, tweet.likeCount])
-        # sentiment = analyzer.polarity_scores(tweet.content)
-        # print(tweet.content)
-        # print(sentiment)
-        # print("-------------------------------------------------")
 
 df = pd.DataFrame(tweets_list,columns=["date", "tweets", "retweetcounts", "likecounts"])   
-# print(df)
-# print("-------------------------------------------------")
 
-# q: function to list tweets in decending order of sentiment score
-
+# sort tweets in decending order of sentiment score
 def sort_by_sentiment(df):
     df['sentiment_score'] = df['tweets'].apply(lambda x: analyzer.polarity_scores(x))
     df['sentiment_score'] = df['sentiment_score'].apply(lambda x: x['compound'])
     df['link'] = df['tweets'].str.extract(r'(https?://t\.co/\w+)')
     df = df.sort_values(by='sentiment_score', ascending=False)
     df = df.head(10)[['tweets', 'sentiment_score', 'link']]
-    print(df)
-    return df
 
-df = sort_by_sentiment(df)
+    # Reset the index of the DataFrame
+    df.reset_index(drop=True, inplace=True)
 
-# book_title = df.loc[0,"tweets"]
-# sentiment = analyzer.polarity_scores(book_title)
-# sentiment = df.loc[0, "sentiment_score"]
-# print(book_title)
-# print(sentiment)
+    # Get the title of the first tweet
+    first_tweet_title = df.iloc[0]['tweets']
 
+    return df, first_tweet_title
 
-# import openai
+sorted_df, first_tweet_title = sort_by_sentiment(df)
 
-# openai.api_key = ""
+# create book summary based on tweet title
+openai.api_key = os.environ['OPENAI-API-KEY']
+prompt = "Summarize this in two sentences: " + first_tweet_title
 
-# # "Summarize the item.description in two sentences".
-# prompt = "Summarize this in two sentences: " + book_title
+book_summary = openai.Completion.create(
+  model = "text-davinci-003",
+  prompt = prompt,
+  temperature = 1,
+  max_tokens = 600,
+  top_p = 1,
+  frequency_penalty = 0,
+  presence_penalty = 0
+)
 
-# book_summary = openai.Completion.create(
-#   model = "text-davinci-003",
-#   prompt = prompt,
-#   temperature = 1,
-#   max_tokens = 600,
-#   top_p = 1,
-#   frequency_penalty = 0,
-#   presence_penalty = 0
-# )
+# generate video from book summary and image
+url = "https://api.d-id.com/talks"
+script = book_summary['choices'][0]['text']
+print(script)
 
-# print(book_summary)
+payload = {
+    "script": {
+        "type": "text",
+        "provider": {
+            "type": "microsoft",
+            "voice_id": "Jenny"
+        },
+        "ssml": "false",
+        "input": script
+    },
+    "config": {
+        "fluent": "false",
+        "pad_audio": "0.0"
+    },
+    "source_url": "https://i.postimg.cc/Jn6KqzcF/07ada757-70e8-4282-86d7-aa7b3b09fdf9.jpg"
+}
+headers = {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "authorization": "Bearer " + os.environ['D-ID-BEARER']
+}
 
+response = requests.post(url, json=payload, headers=headers)
+# print(response.text)
 
-# book_title = df.loc[0,"tweets"]
-# sentiment = analyzer.polarity_scores(book_title)
-# print(book_title)
-# print(sentiment)
+#wait for 20 seconds for the video to be generated
+time.sleep(20)
+
+id = response.json()['id']
+# id = "tlk_R-cPzrkz3jXgRJyqq_PjD"
+response = requests.get(url + '/' + id, headers=headers)
+print(response.json())
